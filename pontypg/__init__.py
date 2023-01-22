f6807b6de7b5055d118a4c5fca1cc0838394a9d6
chris sira <cbsira@gmail.com>
2023-01-21 21:28:22 -0500

    add errortrap to top-level imports

--- pontypg/__init__.py
+++ pontypg/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "postgres_provider", "lease_postgres_conn",
     "fetchval", "fetchrow", "fetchrows",
     "fetchone", "fetchmany",
+    "error_trap",
 ]
 
 
+from pontypg.errortrap import error_trap
 from pontypg.fetch import fetchval, fetchrow, fetchrows, fetchone, fetchmany
 from pontypg.provider import postgres_provider, lease_postgres_conn
